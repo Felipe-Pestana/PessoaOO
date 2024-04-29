de44b1f7873fdf1f41cec41a746ee299894f250d
Felipe-Pestana <pestana1984@gmail.com>
2024-04-29 13:22:36 -0300

    Add project files.

---
+++ Pessoa/Pessoa.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Pessoa
+{
+    public string Nome;
+
+    public string Sobrenome;
+
+    public int Idade;
+
+    public string Telefone;
+
+    public string Celular;
+
+    public string Email;
+    public Pessoa()
+    {
+
+    }
+
+    public string DefinirNome()
+    {
+        Console.WriteLine("Informe o nome: ");
+
+        //string nome;
+        //nome = Console.ReadLine();
+        //return nome;
+
+        return Console.ReadLine();
+    }
+    public string DefinirSobrenome()
+    {
+        Console.WriteLine("Informe o sobrenome");
+        return Console.ReadLine();
+    }
+    public int DefinirIdade()
+    {
+        Console.WriteLine("Informe a idade");
+        //int idade;
+        //idade = int.Parse(Console.ReadLine());
+        //return idade;
+
+        return int.Parse(Console.ReadLine());
+    }
+
+    public void DefinirTelefone(string telefone)
+    {
+        this.Telefone = telefone;
+    }
+
+    public void DefinirCelular(string celular)
+    {
+        Celular = celular;
+    }
+
+    public void DefinirEmail(string email)
+    {
+        Email = email;
+    }
+
+    public void MostrarDados()
+    {
+        Console.WriteLine($"\n\nNome: {Nome}", this.Nome);
+        Console.WriteLine($"Sobrenome: {Sobrenome}",this.Sobrenome);
+        Console.WriteLine("Idade: "+this.Idade);
+        Console.WriteLine("Telefone: "+this.Telefone);
+        Console.WriteLine($"Celular: {Celular}",this.Celular);
+        Console.WriteLine($"Email: {Email}",this.Email);
+    }
+}
---
+++ Pessoa/Program.cs
@@ -0,0 +1,44 @@
+﻿string telefone, celular, email;
+
+Pessoa p1 = new Pessoa();
+Pessoa p2 = new Pessoa();
+Pessoa p3 = new Pessoa();
+
+Pessoa CriarPessoa()
+{
+    Pessoa p = new Pessoa();
+
+    p.Nome = p.DefinirNome();
+    p.Sobrenome = p.DefinirSobrenome();
+    p.Idade = p.DefinirIdade();
+
+    Console.WriteLine("Informe o telefone:");
+    telefone = Console.ReadLine();
+
+    p.DefinirTelefone(telefone);
+
+    Console.WriteLine("Informe o Celular:");
+    celular = Console.ReadLine();
+
+    p.DefinirCelular(celular);
+
+    Console.WriteLine("Informe o Email:");
+    email = Console.ReadLine();
+
+    p.DefinirEmail(email);
+
+    return p;
+}
+
+p1 = CriarPessoa();
+p2 = CriarPessoa();
+p3 = CriarPessoa();
+
+Console.WriteLine("Primeiro Cadastro");
+p1.MostrarDados();
+
+Console.WriteLine("Segundo Cadastro");
+p2.MostrarDados();
+
+Console.WriteLine("Terceiro Cadastro");
+p3.MostrarDados();
